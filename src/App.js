2637175401c5993e9b304de6d307d04165f4883c
Jake Shepherd <jakeshepherd98@gmail.com>
2020-07-27 15:06:03 +0100

    Make more useful

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import Navbar from 'react-bootstrap/Navbar'
 function App() {
     return (
         <div className="App">
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Projects</Navbar.Brand>
+            <Navbar>
+                <a href="#home">Projects </a>
+                <a href="#contact">Contact me</a>
             </Navbar>
+
             <header className="App-header">
                 <h1>Jake Shepherd</h1>
             </header>
