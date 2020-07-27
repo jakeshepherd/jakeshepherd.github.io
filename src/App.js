410d6bec66397bdc4ba2596af29eff2fe18ab743
Jake Shepherd <jakeshepherd98@gmail.com>
2020-07-27 15:00:21 +0100

    Start navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar'
 
 function App() {
     return (
         <div className="App">
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">Projects</Navbar.Brand>
+            </Navbar>
             <header className="App-header">
                 <h1>Jake Shepherd</h1>
             </header>
