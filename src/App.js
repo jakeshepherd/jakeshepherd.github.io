3058f24438ee945ee7734ae39c2573f2d4d7bd2f
Jake Shepherd <jakeshepherd98@gmail.com>
2020-07-27 14:45:08 +0100

    First stuff

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Jake Shepherd</h1>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Jake Shepherd</h1>
+            </header>
+        </div>
+    );
 }
 
 export default App;
