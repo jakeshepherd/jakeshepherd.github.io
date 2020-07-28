085e4046bffb3edc1f0a71e31336cd89b7c79921
Jake Shepherd <jakeshepherd98@gmail.com>
2020-07-28 10:30:11 +0100

    Add routing to navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import './App.css';
-import Navbar from 'react-bootstrap/Navbar'
+import Nav from "./Nav";
 
 function App() {
     return (
-        <div className="App">
-            <Navbar>
-                <a href="#home">Projects </a>
-                <a href="#contact">Contact me</a>
-            </Navbar>
-
-            <header className="App-header">
-                <h1>Jake Shepherd</h1>
-            </header>
-        </div>
+        <Nav />
     );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <h1>Jake Shepherd</h1>
+    );
+}
+
+export default Home;
