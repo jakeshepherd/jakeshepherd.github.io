ac3b5daef0da59d75bbdc8471162a81a5793b2e8
Jake Shepherd <jakeshepherd98@gmail.com>
2020-08-02 17:20:37 +0100

    Oh goodness whats happening

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,15 +3,22 @@ import Skills from "../Components/Skills";
 
 import '../CSS/Home.css';
 
+import JakeBarry from "../Images/Jake&Barry.jpg";
+
 class Home extends Component {
     render() {
         return (
             <div>
+                <div className={"portrait"}>
+                    <img className={"me"} src={JakeBarry} alt={"Sorry, this could not be found."}/>
+                </div>
                 <h1>Hi there, I'm Jake</h1>
-                <p>I'm a Student & Software Developer studying at the University of Bath for my Bsc in Computer Science.</p>
-                <span className="spacer" />
+                <p>I work as a front end and back end developer. I have passion for tidy, well architected and Test
+                    Driven software.</p>
+                <button>Contact me</button>
+                <span className="spacer"/>
                 <h1>Skills</h1>
-                <Skills skills={["hello", "there"]}/>
+                <Skills skills={["Code", "Ing"]}/>
             </div>
         );
     }
