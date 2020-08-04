8161cc8c87fb73b3e157d6bedd8afb14161cf116
Jake Shepherd <jakeshepherd98@gmail.com>
2020-08-04 20:08:14 +0100

    nicer colour

--- src/Components/RightNav.jsx
+++ src/Components/RightNav.jsx
@@ -10,7 +10,8 @@ const Ul = styled.ul`
 
   @media (max-width: 768px) {
     flex-flow: column nowrap;
-    background-color: #0D2538;
+    background-image: linear-gradient(to bottom, mediumpurple, dodgerblue);
+    border-radius: 10px;
     position: fixed;
     transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(100%)'};
     top: 0;
