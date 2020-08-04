208aede860f680c941b02bf485e686c6567bfebe
Jake Shepherd <jakeshepherd98@gmail.com>
2020-08-05 00:18:35 +0100

    Setup stuff

---
+++ src/Pages/CV.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import "../CSS/CV.css";
+
+class CV extends Component {
+    render() {
+        return (
+            <h1>CV!</h1>
+        );
+    }
+}
+
+export default CV
