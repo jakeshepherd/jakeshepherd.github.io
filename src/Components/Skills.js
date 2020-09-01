2ce6d98e560fd5795c8f33d0a4d6581dc15569a8
Jake Shepherd <jakeshepherd98@gmail.com>
2020-09-01 17:04:29 +0100

    Update alt for image

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -9,7 +9,7 @@ class Skills extends Component {
                 {this.props.skills.map((skill, index) => {
                     return (
                         <div className={"skill-box"} key={index}>
-                            <img id={"skill-icon"} src={skill.image} alt={"it no workey lols"}/>
+                            <img id={"skill-icon"} src={skill.image} alt={"Unable to load photo."}/>
                             <h3>{skill.name}</h3>
                             <p>{skill.description}</p>
                         </div>
