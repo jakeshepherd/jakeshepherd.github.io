a864772583adbf0a2c964c82e59db9542f8daa7f
Jake Shepherd <jakeshepherd98@gmail.com>
2020-09-01 17:04:44 +0100

    Update homepage skills

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,9 +5,9 @@ import ProjectImage from "../Components/ProjectImage";
 import '../CSS/Home.css';
 
 import JakeBarry from "../Images/Jake&Barry.jpg";
-import Communication from "../Images/icons8-communication-skill-64.png";
+import Folder from "../Images/icons8-code-64.png";
 import Coding from "../Images/icons8-laptop-64.png";
-import CleanCode from "../Images/icons8-code-64.png";
+import Facilitation from "../Images/icons8-communicate-64.png";
 import ContactButton from "../Components/ContactButton";
 
 class Home extends Component {
@@ -25,9 +25,9 @@ class Home extends Component {
                 <h1 className={"home-headings"}>Skills</h1>
                 <Skills
                     skills={[
-                        {name: "Test Driven Development", image: Communication, description: "Throughout my placement year I have been mentored by my peers and taken a training course in Test Driven Development. It is something I am passionate about and I believe it creates a more robust codebase."},
+                        {name: "Test Driven Development", image: Folder, description: "Throughout my placement year I have been mentored by my peers and taken a training course in Test Driven Development. It is something I am passionate about and I believe it creates a more robust codebase."},
                         {name: "Full Stack Development", image: Coding, description: "I have dedicated time to further my skills in both back end and front end languages. I am competent in PHP frameworks like Laravel, Javascript frameworks like React and database languages like mySQL."},
-                        {name: "Clean code", image: CleanCode, description: "I have a passion for clean, well architected code and I have experience using MVC as one of many ways to effectively architect a project."},
+                        {name: "Facilitation", image: Facilitation, description: "I have a passion for effective communication in a work environment, as such I have trained and become a Certified ScrumMaster and have put that to use over my year in industry."},
                     ]}
                 />
                 <span className="spacer"/>
@@ -43,8 +43,7 @@ class Home extends Component {
                 <br />
                 <a target="_blank" href="https://icons8.com/icons/set/laptop">Laptop icon</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
                 <br />
-                <a target="_blank" href="https://icons8.com/icons/set/code">Code icon</a>icon by <a target="_blank" href="https://icons8.com">Icons8</a>
-            </div>
+                <a target="_blank" href="https://icons8.com/icons/set/talk-male">Communicate icon</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>            </div>
         );
     }
 }
